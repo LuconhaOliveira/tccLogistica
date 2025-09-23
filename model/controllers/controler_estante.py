312a6b74233f5d694dc0d3d559eeee7fc84c414d
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-09-23 16:19:16 -0300

    ADD: Conexão com o banco de dados para criar a estante

---
+++ model/controllers/controler_estante.py
@@ -0,0 +1,29 @@
+from data.conexao import Conection
+from flask import session
+
+class Estante:
+
+    # Conexao com o banco de dados para criar uma nova estante
+    def cadastrar_estante(enderecamento, estante, linha, coluna, cpf):
+            
+        conexao = Conection.create_connection()
+
+        cursor = conexao.cursor()
+
+        sql = """INSERT INTO estante (
+                        enderecamento, 
+                        estante,
+                        linha,
+                        coluna,
+                        cpf)
+                    VALUES (
+                        %s, %s, %s, %s, %s)"""
+
+        valores = (enderecamento, estante, linha, coluna, cpf)
+
+        cursor.execute(sql, valores)
+
+        conexao.commit()
+
+        cursor.close()
+        conexao.close()
