0b948a16ecc749c4e91e77488ff1261dd6a2ac4a
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-09-30 13:52:18 -0300

    ADD: Conexão com o banco de dados para inserir um tipo com base em uma categoria

--- model/controllers/controler_categorias.py
+++ model/controllers/controler_categorias.py
@@ -23,3 +23,24 @@ class Categoria:
 
         cursor.close()
         conexao.close()
+
+    # Conexao com o banco de dados para criar um tipo com base em uma categoria
+    def cadastrar_tipo_categoria(nome, cod_categoria):
+            
+        conexao = Conection.create_connection()
+
+        cursor = conexao.cursor()
+
+        sql = """INSERT INTO tipo (
+                        nome, cod_categoria)
+                    VALUES (
+                        %s, %s)"""
+
+        valores = (nome, cod_categoria)
+
+        cursor.execute(sql, valores)
+
+        conexao.commit()
+
+        cursor.close()
+        conexao.close()
