4de90a315f6a309de3f65b1e23c9589c8e21a998
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-10-06 15:47:19 -0300

    MODIFY: Retirando o comentario de uma linha de código e removendo uma página de teste html

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ app.secret_key = "ch@v3s3cr3t4444&&@"
 def pagina_principal():
     estantes = Estante.buscar_estantes()
 
-    # if estantes is None:
-    #     estantes = []
+    if estantes is None:
+        estantes = []
 
     filtros = [i["categoria"] for i in estantes]
     filtros = list(set(filtros))
