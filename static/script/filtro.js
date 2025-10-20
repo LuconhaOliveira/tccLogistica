694ace4b2b6d371c2a5091f85f5ccd7e3b0ad7aa
liviacamargoap <livia.camargo122@gmail.com>
2025-10-20 11:29:44 -0300

    ADD: Class no btn no js

--- static/script/filtro.js
+++ static/script/filtro.js
@@ -47,7 +47,7 @@ function alterar_filtros(filtros){
                 <label for=${filtro}>${filtro}</label><br />`;
     });
 
-    checkbox+=`<button type="reset">Limpar filtro</button>`
+    checkbox+=`<button class="card-section--btnLimparFiltro" type="reset">Limpar filtro</button>`
 
     document.querySelector('#filtros').innerHTML = '';
     document.querySelector('#filtros').innerHTML = checkbox;
