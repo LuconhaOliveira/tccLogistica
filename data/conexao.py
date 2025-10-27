3511b1cdb298a3e762e3f71e24325431fc93e6b7
MaitheRomagnoli <matheuszothavio@gmail.com>
2025-10-27 09:46:54 -0300

    MODIFY: alteração de rota e função para acessar e incrementar os produtos

--- data/conexao.py
+++ data/conexao.py
@@ -6,11 +6,11 @@ class Conection:
     # Definimos as credenciais para o banco de dados local.
     # Essas variáveis de classe são usadas para manter a configuração de forma organizada.
 
-    # _HOST = 'localhost'
-    # _DATABASE = 'tcc_logistica'
-    # _USER = 'root'
-    # _PASSWORD = 'root'
-    # _PORT = 3306
+    _HOST = 'localhost'
+    _DATABASE = 'tcc_logistica'
+    _USER = 'root'
+    _PASSWORD = 'root'
+    _PORT = 3306
     
     # "Descomente" o código a baix e comente o código a cima para trocar entre banco de dados online e local
     
