a775408d5d0032158851f278a67e80d20469545e
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-11-10 11:24:42 -0300

    MODIFY: Verificação para saber se o usuario selecionou uma estante antes de selecionar uma linha/coluna

--- app.py
+++ app.py
@@ -270,6 +270,7 @@ def cadastrar_produto():
     tipo = Categoria.recuperar_tipo(cpf)
     caracteristica = Categoria.recuperar_caracteristica(cpf)
     estante = Estante.recuperar_estante(cpf)
+    tipo_categoria = Categoria.recuperar_tipo(cpf)
 
     # Renderiza o template, passando os dados para os selects
     return render_template(
@@ -277,7 +278,8 @@ def cadastrar_produto():
         categoria=categoria, 
         tipo=tipo, 
         caracteristica=caracteristica, 
-        estante=estante
+        estante=estante,
+        tipo_categoria = tipo_categoria
     )
 
 # CADASTRAR PRODUTOS ------------------------------------------------------------------------------------------------------#
